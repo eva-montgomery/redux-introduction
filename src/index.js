a5e25f47980bd11a3ff1cc6d780850d53fc08b9c
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-10 11:07:29 -0500

    adds counter reducer function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// console.log('it is working')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// // "The bank" - state
+// // Describe the ideal version of state
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// {
+//     amount: 100 
+// }
+
+// // if we added 1 to the amount, what would state look like?
+
+// {
+//     amount: 101
+// }
+
+
+// // "A transaction slip" - action
+
+// {
+//     type: 'INCREMENT'
+// }
+
+// {
+//     type: 'DECREMENT'
+// }
+
+// {
+//     type: '🏄'
+// }
+
+
+import {
+    createStore
+} from 'redux';
+
+// "The teller" - reducer function
+// reducers are always named for the state they manage
+// They always receive the currect state and the action they're processing
+const defaultState = { amount: 100 }
+
+function counter(state=defaultState, action) {
+    console.table(action)
+    const newState = {...state};
+    if (action.type === 'INCREMENT') {
+        newState.amount = state.amount + 1;
+    } else if (action.type === 'DECREMENT') {
+     newState.amount = state.amount -1;
+    } else {
+        // no neeed to do anything
+        // we already made a copy of thate to return
+    }
+    // They *must* return the new version of state.
+    return newState;
+}
+
+// You give it a reducer, it gives you back a "store".
+// The store is an object that manages your store using reducer
+const store = createStore(counter);
+// "Push notifications" - subscribe to changes in the store
+store.subscribe(() => {
+    console.log(`The state is now:`);
+    // console.table(store.getState());
+    console.table(store.getState());
+});
+
+// Let's give the store some actions to process
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: '⛷'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
