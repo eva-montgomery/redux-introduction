d39de2975006b523374269c37b104abab463abdd
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-10 11:39:40 -0500

    adds variablesaction types as constants

--- src/index.js
+++ src/index.js
@@ -33,22 +33,58 @@ import {
     createStore
 } from 'redux';
 
+
+// Create your action types as constants so that you get error messages for typos.
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+// write action creator functions.
+// They format your action objects
+// Again, to avoid typos.
+
+function actionIncrement(howMuch=1) {
+    return {
+        type: INCREMENT,
+        amount: howMuch
+    }
+}
+
+function actionDecrement(howMuch=1) {
+    return {
+        type: DECREMENT,
+        amount: howMuch
+    }
+}
+
 // "The teller" - reducer function
 // reducers are always named for the state they manage
 // They always receive the currect state and the action they're processing
 const defaultState = { amount: 100 }
 
 function counter(state=defaultState, action) {
-    console.table(action)
+    console.log('somebody called counter()')
     const newState = {...state};
-    if (action.type === 'INCREMENT') {
-        newState.amount = state.amount + 1;
-    } else if (action.type === 'DECREMENT') {
-     newState.amount = state.amount -1;
-    } else {
-        // no neeed to do anything
-        // we already made a copy of thate to return
+
+    switch(action.type) {
+        case INCREMENT:
+            newState.amount = state.amount + action.amount;
+            break;
+        case DECREMENT:
+            newState.amount = state.amount - action.amount;
+            break;
+        default:
+            break;
     }
+
+
+    // if (action.type === 'INCREMENT') {
+    //     newState.amount = state.amount + action.amount;
+    // } else if (action.type === 'DECREMENT') {
+    //  newState.amount = state.amount - action.amount;
+    // } else {
+    //     // no neeed to do anything
+    //     // we already made a copy of thate to return
+    // }
     // They *must* return the new version of state.
     return newState;
 }
@@ -64,21 +100,31 @@ store.subscribe(() => {
 });
 
 // Let's give the store some actions to process
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(actionIncrement());
+store.dispatch(actionDecrement());
 
-store.dispatch({
-    type: '⛷'
-});
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(actionIncrement(5));
+store.dispatch(actionDecrement(99));
+ 
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+
+// store.dispatch({
+//     type: 'INCREMENT',
+//     amount: 'garbage'
+// })
+
+// store.dispatch({
+//     type: '⛷'
+// });
+
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+
+// store.dispatch({
+//     type: 'DECREMENT'
+// });
 
 
 
